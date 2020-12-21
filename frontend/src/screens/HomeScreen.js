a5ea1c969736b4c2f89281377b746a5c39b8ad57
Fernando <fp.amado@icloud.com>
2020-12-21 19:14:51 +0000

    10. Load Products From Backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,44 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function HomeScreen() {
+  /* Create React Hook */
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-      <div className="row center">
-        {/* Map all products */}
-        {data.products.map((product) => (
-          /* Every element inside a map should have a key */
-          /* Make the code modular, putting Product (component Product) in a separate js file */
-          <Product key={product._id} product={product}></Product>
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {/* Map all products */}
+          {products.map((product) => (
+            /* Every element inside a map should have a key */
+            /* Make the code modular, putting Product (component Product) in a separate js file */
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
