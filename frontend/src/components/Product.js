4c3611029a6b131f91350c2bdfaa458b31982a8b
Fernando <fp.amado@icloud.com>
2020-12-22 17:07:36 +0000

    15. Implement Add To Cart Action

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 export default function Product(props) {
@@ -6,13 +7,13 @@ export default function Product(props) {
   return (
     <div>
       <div key={product._id} className="card">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <img className="medium" src={product.image} alt={product.name} />
-        </a>
+        </Link>
         <div className="card-body">
-          <a href={`/product/${product._id}`}>
+          <Link to={`/product/${product._id}`}>
             <h2>{product.name}</h2>
-          </a>
+          </Link>
           {/* Make the code modular, putting Rating (component Rating) in a separate js file */}
           <Rating
             rating={product.rating}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,14 +4,22 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 
-const initialState = {};
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 /* This reducer only returns a list of products,
 "reducer = (state, action)"
 */
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
